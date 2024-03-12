d575c7554d9a0e2f095a7635d893f90c3e6269db
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2024-03-12 17:20:44 +0100

    added some utiles, an errorController and basis of routes

---
+++ grosshop/backend/helpers/httpStatusCodes.js
@@ -0,0 +1,6 @@
+export const OK = 200;
+export const CREATE = 201;
+export const NO_CONTENT = 204;
+export const BAD_REQUEST = 400;
+export const NOT_FOUND = 303;
+export const INTERNAL_SERVER_ERROR = 500;
