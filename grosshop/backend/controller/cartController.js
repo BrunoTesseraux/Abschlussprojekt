76ac7f878414c94a90b5c0450730cc69e200a91d
SuperCoderSuat <supercodesuat@gmail.com>
2024-03-12 17:16:29 +0100

    added controller layer

---
+++ grosshop/backend/controller/cartController.js
@@ -0,0 +1,24 @@
+import { catchAsync } from "../utils/catchAsync.js";
+
+const getCartCtrl = catchAsync(async (req, res, next) => {
+
+});
+
+const postCartCtrl = catchAsync(async (req, res, next) => {
+
+});
+
+const patchCartCtrl = catchAsync(async (req, res, next) => {
+
+});
+
+const deleteCartCtrl = catchAsync(async (req, res, next) => {
+
+});
+
+export const CartController = {
+    getCartCtrl,
+    postCartCtrl,
+    patchCartCtrl,
+    deleteCartCtrl,
+};
---
+++ grosshop/backend/controller/wishlistController.js
@@ -0,0 +1,29 @@
+import { catchAsync } from "../utils/catchAsync.js";
+
+const getOneItemCtrl = catchAsync(async (req, res, next) => {
+
+});
+
+const postOneItemCtrl = catchAsync(async (req, res, next) => {
+
+});
+
+const patchOneItemCtrl = catchAsync(async (req, res, next) => {
+
+});
+
+const deleteOneItemCtrl = catchAsync(async (req, res, next) => {
+
+});
+
+const deleteManyItemsCtrl = catchAsync(async (req, res, next) => {
+
+});
+
+export const WishlistController = {
+    getOneItemCtrl,
+    postOneItemCtrl,
+    patchOneItemCtrl,
+    deleteOneItemCtrl,
+    deleteManyItemsCtrl,
+}
