71bd4aa6d2091bcd599294ac0f147476a1c59b25
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2024-03-16 13:18:41 +0100

    added authentication

--- grosshop/backend/src/services/userService/auth/refreshUserToken.js
+++ grosshop/backend/src/services/userService/auth/refreshUserToken.js
@@ -1,3 +1,9 @@
-export const refreshUserToken = async () => {
-    
-}
+import User from "../../../models/userSchema.js";
+import { createToken } from "../../../utils/jwt.js";
+
+export const refreshUserToken = async (authenticatedUserId) => {
+  const user = await User.findById(authenticatedUserId);
+  if (!user) throw new Error("User not found");
+  const accessToken = createToken(user, "access");
+  return { accessToken };
+};
--- grosshop/backend/src/services/userService/auth/registerUser.js
+++ grosshop/backend/src/services/userService/auth/registerUser.js
@@ -1,16 +1,16 @@
 import User from "../../../models/userSchema.js";
 import AppError from "../../../utils/AppError.js";
 
-export const registerUser = async (newUserInfo) => {
-
+export const registerUser = async (newUserInfo, next) => {
   const foundUser = await User.findOne({
     email: newUserInfo.email,
   });
 
-  if (foundUser) return next(new AppError('User with this email already exists', 409))
+  if (foundUser)
+    return next(new AppError("User with this email already exists", 409));
 
   const user = await User.create(newUserInfo);
   console.log(user);
 
   return user;
-}
+};
